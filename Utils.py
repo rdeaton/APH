b4ae2a76176596c1b1eca406f0d4f926d8388bc4
Robert Deaton <rdeaton@udel.edu>
2011-04-05 14:06:18 -0400

    Added a naive memoizing class, to be used for caching scaled surfaces
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- Utils.py
+++ Utils.py
@@ -1,5 +1,21 @@
 import pygame
 
+class memoize(object):
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            res = self.func(*args)
+            self.cache[args] = res
+            return res
+        except TypeError:
+            return self.func(*args)
+    def __repr__(self):
+        return self.func.__doc__
+
 def load_image(fullname, colorkey=None):
     try:
         image = pygame.image.load(fullname)
