3e24a253b938b62d7dbd5c4242e757706bea69e0
Robert Deaton <rdeaton@udel.edu>
2011-04-06 10:46:04 -0400

    Remove unnecessary conversion
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- Utils.py
+++ Utils.py
@@ -23,7 +23,6 @@ def load_image(fullname, colorkey=None):
     except pygame.error, message:
         print "Cannot load image:", name
         raise SystemExit, message
-    image = image.convert()
     if colorkey is not None:
         if colorkey is -1:
             colorkey = image.get_at((0,0))
