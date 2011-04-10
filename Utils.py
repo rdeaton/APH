bf849de0ed9412325bfa709386e7dbc55852691b
Robert Deaton <rdeaton@udel.edu>
2011-04-10 14:04:31 -0400

    Add a bit of documentation to Utils
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- Utils.py
+++ Utils.py
@@ -1,6 +1,7 @@
 import pygame
 
 class memoize(object):
+    """ This is a decorator to allow memoization of function calls. """
     def __init__(self, func):
         self.func = func
         self.cache = {}
@@ -18,6 +19,9 @@ class memoize(object):
 
 @memoize
 def load_image(fullname, colorkey=None):
+    """ Load an image from a filename. colorkey is an optional parameter, if it
+    is set, then the colorkey of the image is set. If colorkey is -1, then the
+    colorkey is automatically determined from the top left corner. """
     try:
         image = pygame.image.load(fullname)
     except pygame.error, message:
@@ -29,5 +33,6 @@ def load_image(fullname, colorkey=None):
         image.set_colorkey(colorkey, pygame.locals.RLEACCEL)
     return image.convert_alpha()
     
-def dist2(x,y):
-    return (x[0]-y[0]) ** 2 + (x[1]-y[1]) ** 2
+def dist2(pt1,pt2):
+    """ Returns the square of a distance between two points"""
+    return (pt1[0]-pt2[0]) ** 2 + (pt1[1]-pt2[1]) ** 2
