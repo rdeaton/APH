233a83b24e01914a3d8281b260ac15e0b49e14ab
Robert Deaton <rdeaton@udel.edu>
2011-04-22 00:28:54 -0400

    Add Mouse to the list of default imports.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 # This file allows the user to import module APH
 from Game import GameState
 
-__all__ = ['Screen', 'Game', 'Utils', 'Sprite']
+__all__ = ['Screen', 'Game', 'Utils', 'Sprite', 'Mouse']
